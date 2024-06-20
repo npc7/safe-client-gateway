3b50fd2e56e2592e130c1fddcf5c108fbc69571d
max <cao.4321519@163.com>
2024-06-20 21:56:10 +0800

    next

--- src/routes/common/decorators/utils.ts
+++ src/routes/common/decorators/utils.ts
@@ -2,5 +2,5 @@ import { Request } from 'express';
 
 export function getRouteUrl(request: Request): URL {
   const protocol = request.get('X-Forwarded-Proto') ?? request.protocol;
-  return new URL(`${protocol}://${request.get('Host')}${request.originalUrl}`);
+  return new URL(`${protocol}://${request.get('Host')}/cgw${request.originalUrl}`);
 }
